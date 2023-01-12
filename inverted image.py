9c84b57f5d39bd61c27eefd76ee93f8b512909da
sharanyak07 <108124572+sharanyak07@users.noreply.github.com>
2023-01-12 14:14:01 +0530

    Add files via upload

---
+++ inverted image.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+from PIL import Image
+from PIL import ImageFilter
+import os
+
+def main():
+# path of the folder containing the raw images
+    inPath ="D:/images"
+
+# path of the folder that will contain the modified image
+    outPath ="D:/ip1"
+
+    for imagePath in os.listdir(inPath):
+# imagePath contains name of the image
+        inputPath = os.path.join(inPath, imagePath)
+
+# inputPath contains the full directory name
+        img = Image.open(inputPath)
+
+        fullOutPath = os.path.join(outPath, 'invert_'+imagePath)
+# fullOutPath contains the path of the output
+# image that needs to be generated
+        img.rotate(90).save(fullOutPath)
+
+        print(fullOutPath)
+
+# Driver Function
+if __name__ == '__main__':
+    main()
+
